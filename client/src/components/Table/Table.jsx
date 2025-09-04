13f9b29358fc4d4a273ca56698ca85a84fb8fd92
Ameen <mohamed.kaamil.ameen@gmail.com>
2025-09-04 14:44:47 +0530

    table comp commit

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import {useState, useEffect} from "react";
+import inventoryAPI from "../../api/api";
 import {
   Table,
   TableBody,
@@ -10,18 +11,34 @@ import {
 import "./Table.css";
 
 export default function TableList({ items, onEdit, onDelete }) {
+  const [form, setForm] = useState([]);
+  useEffect(()=>{
+    inventoryAPI.getInventory().then((response)=>{
+      const formattedData = response.data.map(item => ({
+        id: item.i_id,
+        name: item.Owner_name,
+        quantity: item.Quantity,
+        location: item.Hub_location
+      }));
+      setForm(formattedData);
+    }).catch((error)=>{
+      console.error("Error fetching inventory data:", error);
+    },
+  )
+  })
+  console.log(form);
   return (
     <Table>
       <TableHead>
         <TableRow>
+          <TableCell>Inventory ID</TableCell>
           <TableCell>Name</TableCell>
           <TableCell>Quantity</TableCell>
-          <TableCell>Location</TableCell>
-          <TableCell>Actions</TableCell>
+          <TableCell>Hub Location</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
-        {items.map((item, index) => (
+        {form.map((item, index) => (
           <TableRow key={index}>
             <TableCell>{item.name}</TableCell>
             <TableCell>{item.quantity}</TableCell>
